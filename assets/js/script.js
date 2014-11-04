7660b4d1385c704a654583355bde345c84ba96b8
aniarya82 <hiten.iitkgp@gmail.com>
2014-11-04 19:48:19 +0530

    new launch website

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+$(function(){
+	
+	var note = $('#note'),
+		ts = new Date(2012, 0, 1),
+		newYear = true;
+	
+	if((new Date()) > ts){
+		// The new year is here! Count towards something else.
+		// Notice the *1000 at the end - time must be in milliseconds
+		fd = new Date(2014, 11, 15, 11, 30, 30, 30) - (new Date()).getTime();
+		// ts = (new Date()).getTime() + 12*24*60*60*1000;
+		ts = (new Date()).getTime() + fd;
+		newYear = false;
+	}
+		
+	$('#countdown').countdown({
+		timestamp	: ts,
+		callback	: function(days, hours, minutes, seconds){
+			
+			var message = "";
+			
+			message += days + " day" + ( days==1 ? '':'s' ) + ", ";
+			message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
+			message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
+			message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
+			
+			if(newYear){
+				message += "left until the new year!";
+			}
+			else {
+				message += "left to Decemer 15, 2014 from now!";
+			}
+			
+			note.html(message);
+		}
+	});
+	
+});
